c171faebfcba85b5d2598812f81e7179811d31be
caiotdandrade <caiotdandrade@gmail.com>
2024-11-01 19:46:29 -0300

    Implements NDCG metric
    - Implements NDCG using binary relevance.
    
    Signed-off-by: caiotdandrade <caiotdandrade@gmail.com>

--- metrics.py
+++ metrics.py
@@ -39,4 +39,50 @@ def RR(rec, actual):
             break
     return 1/idx
 
-        
+def rel(itemId, ground_truth):
+    """
+        ins:
+            - itemId (str)
+            - ground_truth (list)
+        out:
+            - relevance of item with itemId given
+            a ground_truth
+
+        Implements binary relevance: if itemId in ground_truth, return 1. Return 0 elsewhere.
+    """
+
+
+    return itemId in ground_truth
+
+
+def DCG(userRecs, ground_truth):
+    """ 
+        ins:
+            - userRecs: list
+            - ground_truth: list
+        out:
+            DCG for the recommendations generated for the user
+    """ 
+
+    dcg = 0
+    for i, rec in enumerate(user_recs):
+        dcg += (2 ** rel(rec, ground_truth) - 1)   / np.log(i + 1)
+    return dcg
+
+
+def NDCG(userId, recs, ground_truth):
+     """ 
+        ins:
+            - userId: (str)
+            - recs: pd.DataFrame
+            - ground_truth: pd.Dataframe
+        out:
+            NDCG for the recommendations generated for the user
+    """ 
+
+    user_recs = list(recs[recs["userId"] == userId].itemId.values())
+    user_ground_truth = list(ground_truth[ground_truth["userId"] == userId].itemId.values())
+    u_dcg = DCG(user_recs, user_ground_truth)
+    i_dcg = DDG(user_ground_truth, user_ground_truth)
+
+    return u_dcg / i_dcg
