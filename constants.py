92fb7f66a714c5f4e6e703ed222d53b6da41a8cc
Caio Andrade <caiotdandrade@gmail.com>
2025-02-13 20:19:24 -0300

    Adds notebook that generates train/test/val set for movielens

---
+++ constants.py
@@ -0,0 +1,12 @@
+# Movielens constants
+
+BASE_PATH = "datasets/movielens20m"
+FULL_PATH = f"{BASE_PATH}/full.csv"
+TRAIN_FEATURES_PATH = f"{BASE_PATH}/train_x.csv"
+TRAIN_TARGET_PATH = f"{BASE_PATH}/train_y.csv"
+
+TEST_FEATURES_PATH = f"{BASE_PATH}/test_x.csv"
+TEST_TARGET_PATH = f"{BASE_PATH}/test_y.csv"
+
+VAL_FEATURES_PATH = f"{BASE_PATH}/val_x.csv"
+VAL_TARGET_PATH = f"{BASE_PATH}/val_y.csv"
